d1be9d84fe3529e460da0c266039055eb468716a
Petterson <petterson@email.com>
2024-12-02 16:14:29 -0300

    desafio 02.09 - variaveis e operadores aritmeticos

---
+++ 02.09. Desafio - variáveis e operadores aritméticos/DesafioOperadoresAritmeticos.java
@@ -0,0 +1,13 @@
+public class DesafioOperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        int ultimaVenda = 20;
+        int penultimaVenda = 30;
+        int antePenultimaVenda = 100;
+
+        int mediaTicket = (ultimaVenda + penultimaVenda + antePenultimaVenda) / 3;
+
+        System.out.println(mediaTicket);
+    }
+
+}
