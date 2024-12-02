be3cc1e3dab8b1aa959b27a494963435843b7406
Petterson <petterson@email.com>
2024-12-02 15:13:10 -0300

    comentário de linha e comentário de bloco

--- 02.01. Criando o primeiro programa Java/OlaMergulhador.java
+++ 02.01. Criando o primeiro programa Java/OlaMergulhador.java
@@ -1,5 +1,10 @@
-public class OlaMergulhador {
-
+//Comentário de linha (inteira)
+public class OlaMergulhador {//Comentário (de linha) na frente de declaração ou instrução
+    /* 
+    Obs.: Comentário de bloco
+    Método principal
+    Ao executar o programa, esse método será chamada automaticamente.
+    */
     public static void main(String[] args){
         System.out.println("Olá, mergulhador!");
     }
