fd95c2ae65d437d611affa40d27b58cff0681830
Petterson <petterson@email.com>
2025-01-07 15:57:59 -0300

    desafio 20.14 - Text blocks e expressões regulares

---
+++ src/20.14. Desafio - Text blocks e expressões regulares/src/com/github/ej/JsonScraper.java
@@ -0,0 +1,41 @@
+package com.github.ej;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JsonScraper {
+
+    public static void main(String[] args) {
+        String json = """
+            {
+                "id": 1,
+                "nome": "João da Silva"
+            },
+            {
+                "id": 2,
+                "nome": "Maria Abadia"
+            },
+            {
+                "id": 3,
+                "nome": "Sebastião Carvalho"
+            }
+            """;
+
+
+        String regex = ".*?\"nome\":\\s*\"(.*?)\".*?";
+
+        //Ou usando grupo nomeado:
+        //String regex = ".*?\"nome\":\\s*\"(?<nome>.*?)\".*?";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(json);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+            //System.out.println(matcher.group("nome"));
+        }
+
+    }
+
+}
