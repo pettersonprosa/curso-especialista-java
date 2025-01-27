5fcac7d776c408cf2dd93df485ad626a816d0a37
Petterson <petterson@email.com>
2025-01-27 18:54:51 -0300

    desafio 28.18 - API clássica de I-O (codigo do professor)

---
+++ src/28.18. Desafio - API clássica de I-O/src/com/github/ej/Principal1.java
@@ -0,0 +1,23 @@
+package com.github.ej;
+
+import com.github.ej.io.FragmentadorDeArquivo;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Principal1 {
+
+    public static void main(String[] args) {
+        try {
+            var fragmentador = new FragmentadorDeArquivo(
+                new File("arquivos/fotos/picanha.jpg"),
+                1024 * 50);
+            fragmentador.fragmentar();
+        } catch (IOException e) {
+            System.out.println("Erro fragmentando arquivo");
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/28.18. Desafio - API clássica de I-O/src/com/github/ej/Principal2.java
@@ -0,0 +1,24 @@
+package com.github.ej;
+
+import com.github.ej.io.UnificadorDeArquivos;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Principal2 {
+
+    public static void main(String[] args) {
+        try {
+            var unificador = new UnificadorDeArquivos(
+                new File("arquivos/fotos"),
+                new File("arquivos/fotos/picanha-unificado.jpg"),
+                "picanha.jpg");
+            unificador.unificar();
+        } catch (IOException e) {
+            System.out.println("Erro ao unificar arquivos");
+            e.printStackTrace();
+        }
+
+    }
+
+}
