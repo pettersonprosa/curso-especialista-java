2087714a21c9a68ba38718d5d4fec69d3407d0f7
Petterson <petterson@email.com>
2025-01-28 14:29:26 -0300

    desafio 29.21 - NIO.2 (codigo do professor)

---
+++ src/29.21. Desafio - NIO.2/src/com/github/ej/Principal1.java
@@ -0,0 +1,22 @@
+package com.github.ej;
+
+import com.github.ej.io.FragmentadorDeArquivo;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class Principal1 {
+
+    public static void main(String[] args) {
+        try {
+            var fragmentador = new FragmentadorDeArquivo(
+                    Path.of("arquivos/fotos/picanha.jpg"),
+                    1024 * 50);
+            fragmentador.fragmentar();
+        } catch (IOException e) {
+            System.out.println("Erro fragmentando arquivo");
+            e.printStackTrace();
+        }
+    }
+
+}
